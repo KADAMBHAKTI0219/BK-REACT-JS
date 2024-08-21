18ad3ff9eb4fb76f5958a3eb5d181cdb81ac02ae
KADAM BHAKTI <kadambhakti0219@gmail.com>
2024-08-21 21:58:09 +0530

    completed

--- Firebase Storage/firebase-storage/src/Components/AllRoutes.jsx
+++ Firebase Storage/firebase-storage/src/Components/AllRoutes.jsx
@@ -5,16 +5,19 @@ import Description from './Description'
 import EditProducts from './EditProducts'
 import AddProducts from './AddProducts'
 import HomePage from './HomePage'
+import PrivateAuthentication from './PrivateAuthentication'
+import LoginPage from '../Pages/LoginPage'
 
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
-        <Route path='/products' element={<ProductsPage/>}/>
+        <Route path='/products' element={<PrivateAuthentication><ProductsPage/></PrivateAuthentication>}/>
         <Route path='/description/:id' element={<Description/>}/>
         <Route path='/editPage/:id' element={<EditProducts/>}/>
         <Route path='/addProducts' element={<AddProducts/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
       </Routes>
     </div>
   )
--- Firebase Storage/firebase-storage/src/Services/Firebase.js
+++ Firebase Storage/firebase-storage/src/Services/Firebase.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDmF-S4RaeGCWL1co6uXgIBLYnI4vvMJFg",
   authDomain: "bk-react-0219.firebaseapp.com",
@@ -19,4 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app)
