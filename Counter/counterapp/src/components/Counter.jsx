d80f80a9afddea6b9ff49bc6d75488e575987ce3
KADAM BHAKTI <kadambhakti0219@gmail.com>
2024-08-22 18:38:03 +0530

    completed

--- Counter/counterapp/src/components/Counter.jsx
+++ Counter/counterapp/src/components/Counter.jsx
@@ -5,6 +5,19 @@ const Counter = () => {
     const handleCount = (value)=>{
       setCount(count + value)
     }
+    const evenOrOdd = count % 2 == 0 ? "Even" : "Odd"
+    const PrimeNumber = (num)=>{
+    let factor = 0
+    for (let i = 1; i <= num ; i++) {
+      if (num % i === 0) {
+        factor++
+        console.log(factor)
+      }
+  } return factor
+}
+let primeNo =  PrimeNumber(count)
+ 
+
   return (
     <div style={{width:"40%",border:"4px solid black",margin:"15% auto",padding:"20px 10px",borderRadius:"20px"}}>
         <h1>COUNTER</h1>
@@ -12,6 +25,8 @@ const Counter = () => {
         <span style={{fontSize:"28px"}}>{count}</span>
         <button onClick={()=>handleCount(1)} style={{padding:"8px 16px",backgroundColor:"green",color:"white",fontSize:"20px",borderRadius:"10px",marginLeft:"30px"}}> INCREMENT(+) </button>
         <button onClick={()=>setCount(0)} style={{padding:"8px 16px",backgroundColor:"orange",fontSize:"20px",borderRadius:"10px",display:"block",margin:"20px auto"}}>RESET</button>
+        <h1>this number is {evenOrOdd}</h1>
+        <h1>This number is {primeNo == 2 ? "Prime" :"Not Prime"}</h1>
     </div>
   )
 }
